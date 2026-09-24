b71fa3fee9b429e74453ccf49ec13532012c92ec
Darya Ivanova <B003314@burservis.ru>
2026-09-24 10:51:24 +0500

    feat: update cache on update price

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -27,11 +27,11 @@ namespace PriceService.Services
         public async Task<Product?> GetByIdAsync(int id)
         {
             var key = "product:" + id;
-            var cashedValue = await _redis.StringGetAsync(key);
+            var cachedValue = await _redis.StringGetAsync(key);
 
-            if (cashedValue != RedisValue.Null)
+            if (cachedValue != RedisValue.Null)
             {
-                return JsonSerializer.Deserialize<Product>(cashedValue.ToString());
+                return JsonSerializer.Deserialize<Product>(cachedValue.ToString());
             }
 
             var product = await _db.Products.FindAsync(id);
@@ -57,6 +57,36 @@ namespace PriceService.Services
             _db.Products.Add(product);
             await _db.SaveChangesAsync();
 
+            return product;
+        }
+
+        /// <summary>
+        /// Обновляет цену товара в БД.
+        /// </summary>
+        /// <remarks>Если товар был кэширован, обновляем значение в redis.</remarks>
+        /// <param name="id">Идентификатор товара.</param>
+        /// <param name="price">Новая цена.</param>
+        /// <returns>Актуальные данные о товаре.</returns>
+        public async Task<Product?> UpdatePriceAsync(int id, decimal price)
+        {
+            var product = await _db.Products.FindAsync(id);
+
+            if (product == null)
+                return null;
+
+            product.Price = price;
+            await _db.SaveChangesAsync();
+
+            var key = "product:" + id;
+            var cachedValue = await _redis.StringGetAsync(key);
+
+            if (cachedValue != RedisValue.Null)
+            {
+                await _redis.StringSetAsync(
+                    key,
+                    JsonSerializer.Serialize(product),
+                    TimeSpan.FromMinutes(5));
+            }
             return product;
         }
     }
